977f5df4187f059fa72dda157fb617f03dbcb2e3
Ryan Ackermann <ryha26@gmail.com>
2016-01-04 13:59:07 -0600

    added the basic project files

---
+++ Quakes/Quakes/ViewController.swift
@@ -0,0 +1,15 @@
+
+import UIKit
+
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(white: 0.2, alpha: 1.0)
+    }
+    
+}
+
---
+++ Quakes/Quakes/WindowController.swift
@@ -0,0 +1,26 @@
+
+
+import UIKit
+
+
+@UIApplicationMain
+class WindowController: UIResponder, UIApplicationDelegate
+{
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+
+}
+
